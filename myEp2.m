094e7baa51ad36da9610dcd1850d0862119d7a5c
Alyssa <florencealyssa@gmail.com>
2015-05-15 12:54:00 -0300

    Resta implementar a funcao que inverte a matriz B

--- myEp2.m
+++ myEp2.m
@@ -9,8 +9,8 @@
 
 #=======================================================================
 # Teste com uma solucao viavel basica otima: (extraida do livro p.88)
-#  A = [1,1,1,1 ; 2,0,3,4],  x = [1;1;0;0] ,  b = [2;2],
-#  c = [2,0,0,0] , m = 2 , n = 4
+# A = [1,1,1,1 ; 2,0,3,4]  x = [1;1;0;0]   b = [2;2]
+# c = [2;0;0;0]  m = 2  n = 4
 #
 #
 # Teste p.55
@@ -30,29 +30,25 @@
 function [ind, v] = simplex(A, b, c, m, n, x)
    ind = 2;   #dummy
    count = 0;
-   v = zeros(n,1);        
 
   [basic, Nbasic] = indBasico(x, n);
-  [B,cb] = matrizB(A, basic, c, m, n, x);
-
-  invB = inv(B);
+  [B, InvB, cb] = matrizB(A, basic, c, m, n, x);
 
   while(ind == 2)
     printIter(count);
+    printBasic(basic, x);
     printCost(c, x);
 
-    cost = custo(A, invB, Nbasic, c, cb, m, n, x);
- #   j  = checkNegativo(cost, n) ;
+    cost = custo(A, InvB, Nbasic, c, cb, m, n, x);
+    [ind, v, j] = direction(A, InvB, cost, m, n, x);
 
-    if (j != 0)                    #Existe uma direcao que diminui o custo
-   [ind, v, x] = direction(A, invB, basic, Nbasic,cost, m, n, x);
-
-    else                           #A solucao e otima
-      ind = 0;    fim = 1;
-      v = x;
+    if (j != 0)
+      [B, InvB, basic, Nbasic, x] = newB(B, InvB, basic, Nbasic, v, j, x, m, n);
     endif
+    
     count++;
   end
+  
   printf("\n");
 endfunction
 
@@ -67,14 +63,12 @@ function [basic, Nbasic] = indBasico(x, n)
   basic = zeros(n, 1);    k = 1;
   Nbasic = zeros(n, 1);   s = 1;   
 
-  printf("Variaveis basicas:\n");
   for (i = 1 : n)
     if (x(i) == 0)
       Nbasic(k++) = i;
 
     else
       basic(s++) = i;
-      printf("%d:  %f\n", i, x(i));
     endif
   end  
 endfunction
@@ -84,10 +78,10 @@ endfunction
 #=======================================================================
 # Esta funcao recebe uma matriz A de tamanho m x n, vetores c e x de  
 # tamanho n e o vetor basic com indices das variaveis basicas de x.
-# Devolve uma matriz B com as colunas basicas de A, associado a solucao 
-# x e cb, o vetor de custo dos indices basicos.
+# Devolve uma matriz B com as colunas basicas de A associado a solucao 
+# x, a matriz inversa de B e cb, o vetor de custo dos indices basicos.
 #=======================================================================
-function [B, cb] = matrizB(A, basic, c, m, n, x) 
+function [B, InvB, cb] = matrizB(A, basic, c, m, n, x) 
   k = 1;  index = basic(k);
 
   B = A(:, index);
@@ -99,6 +93,8 @@ function [B, cb] = matrizB(A, basic, c, m, n, x)
     cb = vertcat(cb, c(index));
     index = basic(++k);
   end
+ 
+  InvB = inv(B);
 endfunction
 
 
@@ -115,7 +111,6 @@ function [cost] = custo(A, B, Nbasic, c, cb, m, n, x)
     p(i) = cb' * B(:, i);
   end
 
-
   printf("\nCustos reduzidos:\n");
   cost = zeros(n,1);
   k = 1;  index = Nbasic(k);  
@@ -125,6 +120,128 @@ function [cost] = custo(A, B, Nbasic, c, cb, m, n, x)
     printf("%d: %f\n", index, cost(index));
     index = Nbasic(++k);
   end  
+endfunction
+
+
+
+#=======================================================================
+# Esta funcao devolve em v a solucao otima caso x seja otimo. 
+# Se x nao for otimo, devolve o indice (k) da variavel nao basica que
+# diminua o valor da funcao de custo. (k = 0 se esta indice nao existir)
+#=======================================================================
+function [ind, v, k] = direction(A, invB, cost, m, n, x)
+  ind = 2;  #dummy
+  k = checkNegativo(cost, n);  
+
+  if (k == 0)     #A solucao e otima
+    ind = 0;  v = x;
+    j = 0;
+    return;
+  endif
+                  
+  v = invB * A(:, k);            
+  j = checkNegativo(-v, n);      
+
+  if (j == 0)
+    printf("O valor da funcao objetivo vai para -Inf\n");
+    ind = -1;
+    return;
+  endif
+
+
+endfunction
+
+
+#=======================================================================
+# Esta funcao gera a matriz B atualizada e associada com a nova solucao 
+# x. Atualiza tambem os indices das novas variaveis basicas e nao basicas.
+#=======================================================================
+function [B, InvB, basic, Nbasic, x] = newB (A, B, InvB, basic, Nbasic, u, j, x, m, n)
+    printf("\nEntra na base: %d\n", j);
+    printDirecao(basic, u);
+
+    [theta, index] = thetaMax(u, basic, m, x);
+    x = newX(theta, basic, u, j, x, m, n);
+    [B, basic, Nbasic] = swap(A, B, basic, Nbasic, index, j, m, n);   
+                                                  #Coluna index de B sai na base
+                                                  #Coluna j de A entra da base
+#   InvB = newInvB(invB, v);
+
+endfunction
+
+
+
+
+
+#=======================================================================
+# A funcao calcula thetaMax (o maximo que se pode andar na direcao 
+# viavel basica respeitando todas as restricoes) e o indice j tal que 
+# thetaMax = min [xB(j) / u(j)], j = 1...m, u(j) > 0
+#=======================================================================
+
+#ATENÇAO PARA A ORDEM DOS INDICES DAS VARIAVEIS BASICAS NAS ITERACOES SEGUINTES
+
+function [theta, j] = thetaMax(u, basic, m, x)
+  theta = Inf;
+ 
+  j = 0;
+  k = 1;   index = basic(k);
+   
+  while (index != 0)
+    if (u(index) > 0)
+      t = (x(index) / u(index));
+      
+      if (t < theta)
+	theta = t;
+        j = index;
+      endif
+    endif
+  index = basic(++k);
+  end
+
+  printf("\nTheta*: %f\n\n", theta);
+  printf("Sai da base: %d\n\n", j);
+endfunction
+
+
+#=======================================================================
+########PASSIVEL DE ERROS(?)
+# A funcao troca a coluna B[sai] pela coluna A[entra] e atualiza os
+# indices basicos e nao basicos
+#=======================================================================
+function [B, basic, Nbasic] = swap(A, B, basic, Nbasic, sai, entra, m, n)
+  k = 1;  index = basic(k); 
+
+  while (index != sai)
+    index = basic(++k);
+  end
+
+  B(:, k) = A(:, entra);        
+  basic(k) = entra;
+
+  k = 1; index = Nbasic(k);
+  while (index != entra)
+    index = basic(++k);
+  end
+  Nbasic(k) = sai;
+
+endfunction
+
+
+
+#=======================================================================
+# A funcao atualiza a antiga solucao basica pela nova que diminui a 
+# funcao de custo.
+#=======================================================================
+function [x] = newX(theta, basic, u, entra, x, m, n)
+
+  k = 1; index = basic(k);  
+  while (index != 0)
+    x(index) -= theta * u(k);
+    index = basic(++k);
+  end
+
+  x(entra) = theta;
 endfunction
 
 
@@ -146,126 +263,19 @@ endfunction
 
 
 #=======================================================================
-# Esta funcao determina se a direcao viavel basica que diminui o valor da 
-# funcao de custo ou se leva a funcao de custo ao -Inf
-#=======================================================================
-function [ind, v, x] = direction(A, invB, basic, Nbasic, cost, m, n, x)
-  j = checkNegativo(cost, n);  
-
-  if (j == 0)     #A solucao e otima
-    ind = 0;      #nao ha direcao basica que diminua o custo
-    v = x;
-    return;
-  endif
-
-  u = invB * A(:, j);
-  s = checkNegativo(-u, n);
-
-  printf("\nEntra na base: %d\n", j);
-  printDirecao(basic, u);
-
-  if (s != 0)                                   
-    [theta, index] = thetaMax(u, basic, m, x);
-    [B] = swap(A, B, basic, index, j, m, n);     #Coluna j de A entra da base
-                                                 #Coluna index de B sai na base
-    x = newX(theta, u, basic, index, j, x, m, n);
-    ind = 2;  #dummy
-    v = x;    fim = 0;
-      
-  else                                           #custo infinito 
-    v = u;
-    ind = -1;
-    return;
-  endif
-endfunction
-
-
-
-#=======================================================================
-# A funcao calcula thetaMax (o maximo que se pode andar na direcao 
-# viavel basica respeitando todas as restricoes) e o indice j tal que 
-# thetaMax = min [xB(j) / u(j)], j = 1...m, u(j) > 0
-#=======================================================================
-
-####Arrumar [xb]i / ui < theta
-
-function [theta, j] = thetaMax(u, basic, m, x)
-  t = zeros(m,1);
-  theta = -Inf;
-
-  k = 1;   index = basic(k);
-  while (index != 0)
-#**    if (u(k) > 0)
-#**       t(index) = (x(index) / u(index));
-#*    endif
-  index = basic(++k);
-  end
-t
-  [theta, j] = minimo(-t, m);
-  printf("\nTheta*: %f\n\n", theta);
-  printf("Sai da base: %d\n\n", j);
-
-endfunction
-
-
-
-#=======================================================================
-# A funcao retorna o indice e o elemento de menor valor do vetor 
-# v de tamanho n. 
-#=======================================================================
-function [min, index] = minimo(v, n)
-  min = v(1);
-  index = 1;
-  
-  for (i = 2 : n) 
-    if (min > v(i))
-      min = v(i);
-      index = i;
-    endif
-  end
-endfunction
-
-
-
-#=======================================================================
-########PASSIVEL DE ERROS(?)
-# A funcao troca a coluna B[sai] pela coluna A[entra]
-#=======================================================================
-function [B] = swap(A, B, basic, sai, entra, m, n)
-  k = 1;  index = basic(k); 
-
-  while (index != 0)
-    if (index == sai)
-      B(:, k) = A(:, entra:entra);        
-      break;
-    endif
-
-    index = basic(++k);
-  end
-endfunction
-
-
-
-#=======================================================================
-# A funcao atualiza a antiga solucao basica pela nova que diminui a 
-# funcao de custo.
-#=======================================================================
-function [x] = newX(theta, db, basic, sai, entra, x, m, n)
-  k = 1;  index = basic(k);
-  
-  while(index != 0)
-    x(index) = x(index) + theta * db(index);
-    index = basic(++k);
-  end
-
-  x(entra) = - theta;
-endfunction
-
-
-
-#=======================================================================
 # Funcoes auxiliares de impressao
 #=======================================================================
+function printBasic(basic, x)
+  printf("Variaveis basicas:\n");
+
+  k = 1; index = basic(k);
+  while (index != 0)
+    printf("%d:  %f\n", index, x(index));
+    index = basic(++k);
+  end
+    printf("\n");
+endfunction
+
 function printIter(count)
     printf("#====================================\n");
     printf("           Iteracao: %d\n", count);
@@ -274,7 +284,7 @@ endfunction
 
 
 function printCost(c, x)
-    printf("Valor funcao objetivo: %f\n", rot90(c) * x);
+    printf("Valor funcao objetivo: %f\n", c' * x);
 endfunction
 
 
@@ -286,5 +296,4 @@ function printDirecao(basic, u)
       index = basic(++k);
     end
 endfunction
-
 
